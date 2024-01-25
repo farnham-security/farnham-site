306cd8d25ed02ca69b6db2387f65126e4a9d8454
Joshua de Wet <141920614+jdewera@users.noreply.github.com>
2024-01-25 16:30:54 +1300

    Refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,19 +13,13 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#1E2952",
-        secondary: "#F1A712",
         body: "#637381",
-        "gray-1": "#F9FAFB",
-        "gray-2": "#F3F4F6",
-        "gray-7": "#CED4DA",
+        grey: "#F9FAFB",
+        primary: "#1E2952",
+        secondary: "#2C3C77"
       },
       boxShadow: {
-        input: "0px 7px 20px rgba(0, 0, 0, 0.03)",
-        form: "0px 1px 55px -11px rgba(0, 0, 0, 0.01)",
-        pricing: "0px 0px 40px 0px rgba(0, 0, 0, 0.08)",
-        1: "0px 1px 3px 0px rgba(166, 175, 195, 0.40)",
-        2: "0px 5px 12px 0px rgba(0, 0, 0, 0.10)",
+        box: "0px 0px 40px 0px rgba(0, 0, 0, 0.08)"
       },
     },
   },
